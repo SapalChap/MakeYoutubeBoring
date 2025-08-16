2e311b8a7badbff83e0985c402815f9440d1f872
SapalChap <chapagainsapal@gmail.com>
2025-08-16 16:56:16 -0400

    Adding makingImagesBoring and makeTitlesBoring. Images is working, titles is not.

--- content.js
+++ content.js
@@ -1,7 +1,46 @@
 
-const imgs = document.getElementsByTagName("img");
+
 const blackImage = chrome.runtime.getURL("BoringYoutubeThumbnail.png");
 
-for (image of imgs) {
-    image.src = blackImage;
+
+function makeImagesBoring(){
+    const imgs = document.getElementsByTagName("img");
+    
+    for (image of imgs) {
+        
+        if (image.src !== blackImage) {
+            image.src = blackImage;
+        }
+     }
 }
+
+
+
+function makeTitlesBoring(){
+
+    const videoTitles = document.querySelectorAll("h3[title], a#video-title")
+    const boringTitles = [];
+
+    for (let titleElement of videoTitles){
+        let originalTitle = titleElement.getAttribute("title") || titleElement.textContent;
+
+        if (originalTitle) { //make lower case
+            let lowerTitle = originalTitle.toLowerCase();
+            titleElement.textContent = lowerTitle;
+            titleElement.setAttribute("title", lowerTitle);
+
+            if (titleElement.hasAttribute("title")) {
+                console.log("Successfully changed to lower case")
+                titleElement.setAttribute("title", lowerTitle);
+            }            
+            
+        }
+    } }
+
+
+makeImagesBoring();
+makeTitlesBoring();
+
+setInterval(makeImagesBoring, 1000);
+setInterval(makeTitlesBoring, 1000);
+
